7bb693b003ef3ea522223b1a97ea1f5cc87d0fc1
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-08 22:33:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
 end
 #COUNT(character_books.character_id)
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, character_books.character_id
+  "SELECT DISTINCT characters.name, character_books.character_id
    FROM characters
    JOIN character_books
    ON characters.id = character_books.character_id
