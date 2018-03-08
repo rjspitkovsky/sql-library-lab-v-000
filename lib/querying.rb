53344b5566ffeb8531bc2081741a5234f026f405
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-08 22:19:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,8 @@ def select_series_title_with_most_human_characters
    FROM series
    JOIN characters
    ON characters.series_id = series.id
-   WHERE characters.species = 'human' COUNT(characters.species) DESC LIMIT 1;"
+   GROUP BY characters.species
+   WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
