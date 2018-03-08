b09e2d37c4532b8728f25b0f7c8dbe6f0c97989b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-08 22:27:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,10 @@ def select_series_title_with_most_human_characters
    GROUP BY characters.species
    WHERE characters.species = 'human';"
 end
-
+#COUNT(character_books.character_id)
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.character_id)
+   FROM characters
+   JOIN character_books
+   ON characters.id = character_books.character_id;"
 end
