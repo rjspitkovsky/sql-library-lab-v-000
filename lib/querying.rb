350a4346890dfb367277613de6bf917e83eae774
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-08 22:29:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,5 +35,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, character_books.character_id
    FROM characters
    JOIN character_books
-   ON characters.id = character_books.character_id;"
+   ON characters.id = character_books.character_id
+   ORDER BY character_books.character_id DESC;"
 end
