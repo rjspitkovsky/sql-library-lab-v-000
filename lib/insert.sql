4f9b67651749ac7229644097863d89930bf88ec4
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-08 21:32:11 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (2, "Second Series", 2, 2);
+
+INSERT INTO subgenres (id, name) VALUES (1, "medieval");
+INSERT INTO subgenres (id, name) VALUES (2, "space opera");
+
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin");
+INSERT INTO authors (id, name) VALUES (2, "Second Author");
